fix using punctual max distance calculation
before was wrong because I was assuming that the distance from door-1 and door was the maximum

src/pressureplate/environment.py
@@ -343,25 +343,30 @@ class PressurePlate(gym.Env):
             if i == curr_room:
                 reward = - np.linalg.norm((np.array(plate_loc) - np.array(agent_loc)), 1) / self.max_dist
             else:
-                nearest_door_dist = float('inf')
-                for j in range(len(self.doors)):
-                    if curr_room==0:
-                        further_point=0,0
-                        door_loc = self.doors[curr_room].x, self.doors[curr_room].y
-                        max_door_dist= np.linalg.norm((np.array(further_point) - np.array(door_loc)),1)
-                        dist_to_door  = np.linalg.norm((np.array(door_loc) - np.array(agent_loc)), 1)                            
-                        if dist_to_door < nearest_door_dist:
-                            nearest_door_dist = dist_to_door
-                    else:
-                        if curr_room <= j:
-                            door_loc = self.doors[j].x, self.doors[j].y
-                            previous_door_loc=self.doors[curr_room-1].x, self.doors[curr_room-1].y
-                            max_door_dist = np.linalg.norm((np.array(previous_door_loc) - np.array(door_loc)),1)
-                            if dist_to_door < nearest_door_dist:
-                                nearest_door_dist = dist_to_door
+                if self.n_agents==2:
+                    door=self.doors[0]
+                    door_loc= door.x[0], door.y[0]            
+                    dist_to_door  = np.linalg.norm((np.array(door_loc) - np.array(agent_loc)), 1) 
+                    further_point=0,6
+                    max_door_dist= np.linalg.norm((np.array(further_point) - np.array(door_loc)),1)
 
-                reward = -len(self.room_boundaries) + curr_room + 1-nearest_door_dist/max_door_dist
-            
+                    reward = -len(self.room_boundaries) + curr_room + 1-dist_to_door/max_door_dist
+
+                elif self.n_agents==4:
+                    further_from_doors=[[0,14],[10,12],[0,6]]
+                    door=self.doors[curr_room]
+                    door_loc= door.x[0], door.y[0]
+                    dist_to_door  = np.linalg.norm((np.array(door_loc) - np.array(agent_loc)), 1)
+                    further_point=further_from_doors[curr_room]
+                    max_door_dist= np.linalg.norm((np.array(further_point) - np.array(door_loc)),1)
+                
+                    reward = -len(self.room_boundaries) + curr_room + 1-dist_to_door/max_door_dist
+
+                else:
+                    #TODO implement for other number of agents
+                    print('distance from doors implemented only for n_agents 2,4 --> see line 366 pressureplate/environment')
+                    reward = -len(self.room_boundaries) + curr_room + 1
+
             rewards.append(reward)
         return rewards
     